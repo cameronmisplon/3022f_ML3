65c131b0ea300c76f3430136a9f04b1343631e32
Cameron Misplon <mspcam001@nightmare.cs.uct.ac.za>
2021-06-16 17:51:33 +0200

    Classifier.py successfully takes in user input

--- Classifier.py
+++ Classifier.py
@@ -2,8 +2,8 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor, Lambda, Compose
-import matplotlib.pyplot as plt
+from torchvision.transforms import ToTensor
+from PIL import Image
 
 training_data = datasets.MNIST(
         root="./",
@@ -11,15 +11,8 @@ training_data = datasets.MNIST(
         download=False,
         transform=ToTensor()
 )
-test_data = datasets.MNIST(
-        root="./",
-        train=False,
-        download=False,
-        transform=ToTensor()
-)
 batch_size = 64
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data,batch_size = batch_size)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class ANNnetwork(nn.Module):
@@ -39,7 +32,7 @@ class ANNnetwork(nn.Module):
         logits =self.linear_relu_stack(x)
         return logits
 
-def train(dataloader, model, lossFunction, optimization):
+def train(dataloader, model, lossFunction, optimization): # adjusts the weights of the neural network using crossentropy loss function as well as performing the backpropogation
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
@@ -50,19 +43,6 @@ def train(dataloader, model, lossFunction, optimization):
         optimization.step()
         if batch % 100 ==0:
             loss, current = loss.item(), batch*len(X)
-def test(dataloader,model):
-    size = len(dataloader.dataset)
-    model.eval()
-    test_loss, correct = 0,0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X.to(device), y.to(device)
-            pred = model(X)
-            test_loss+= lossFunction(pred,y).item()
-            correct +=(pred.argmax(1) ==y).type(torch.float).sum().item()
-    test_loss /= size
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 model = ANNnetwork().to(device)
 lossFunction = nn.CrossEntropyLoss()
@@ -71,5 +51,13 @@ epochs = 10
 for i in range(epochs):
     print(f"Epoch {i+1}\n----------------------")
     train(train_dataloader,model,lossFunction,optimization)
-    test(test_dataloader, model)
+
 print("Done!")
+img_path = input("Please enter a filepath:\n")
+while img_path != "exit": #Handles making predictions on user inputs
+    image = Image.open(img_path)
+    image = ToTensor()(image).unsqueeze(0)
+    image.to(device)
+    pred = model(image)
+    print("Classifier: "+str(pred.argmax(1).type(torch.float).sum().item()))
+    img_path = input("Please enter a filepath:\n")
